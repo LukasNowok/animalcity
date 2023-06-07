7df88f18db82d4eba4063e9e4394201ca91a9e7a
LukasNowok <nowok.lukas@gmail.com>
2023-06-08 00:49:25 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -19,5 +19,5 @@ function setPos(position) {
   background(0);
   fill(255);
   textSize(18);
-  text(nf(lat,2,2) + " " + nf(lng,2,2), 10, height/2);
+  text(nf(lat,2,8) + " " + nf(lng,2,8), 10, height/2);
 }
