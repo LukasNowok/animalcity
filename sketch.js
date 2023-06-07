e040605a7b8e9a0189a74971a9140aceb20e796f
LukasNowok <nowok.lukas@gmail.com>
2023-06-08 00:35:07 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,6 @@ function setup() {
   if (!navigator.geolocation) {
     alert("navigator.geolocation is not available");
   }
-  navigator.geolocation.getCurrentPosition(setPos);
 }
 function draw(){
   navigator.geolocation.getCurrentPosition(setPos);
@@ -18,6 +17,6 @@ function setPos(position) {
   var lng = position.coords.longitude;
   background(0);
   fill(255);
-  textSize(32);
-  text("Current position: " + nf(lat,2,2) + " " + nf(lng,2,2), 10, height/2);
+  textSize(18);
+  text(nf(lat,2,2) + " " + nf(lng,2,2), 10, height/2);
 }
