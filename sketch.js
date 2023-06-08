a34d4f27f085e93b1cedc35830d559d2666a4a55
LukasNowok <nowok.lukas@gmail.com>
2023-06-08 12:44:04 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 // must be in HTTPS
 
-let osc, startPosition, distanceToStart;
+let osc, oscPlaying, oscButton, startPosition, distanceToStart;
 
 function preload(){
   startPosition = getCurrentPosition();
@@ -16,28 +16,40 @@ function setup() {
   osc = new p5.Oscillator('sine');
   osc.amp(0.5, 0);
   osc.freq(200, 0);
+  oscPlaying = 0;
+  oscButton = createButton('sound on/off');
+  oscButton.position(10,10);
+  oscButton.mousePressed(soundOnOff);
 
   // get position once
   if(geoCheck() == true) {
      background(135, 200, 118);
      startPosition = getCurrentPosition();
-     osc.start();
   } else {
      background(234, 51, 35);
   }
   watchPosition(positionChanged);
-  text("animalcity", 10, height/3);
 }
 
 function positionChanged(position){
+    background(135, 200, 118);
     distanceToStart = calcGeoDistance(position.latitude,position.longitude,startPosition.latitude,startPosition.longitude, 'km');
     print("lat: " + position.latitude);
     print("long: " + position.longitude);
     text(nf(position.latitude,2,8) + " " + nf(position.longitude,2,8), 10, height/2);
-    text(distanceToStart, 10, height/1.5);
+    text(distanceToStart*1000, 10, height/1.5);
 }
 
 function positionToFrequency(){
+
+}
+
+function soundOnOff(){
+  if(oscPlaying){
+    osc.start();
+  } else {
+    osc.stop();
+  }
 
 }
 
