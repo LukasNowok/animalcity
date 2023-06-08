ff4caf83ff1e4b651d4b7c9db720a361eb5a7948
LukasNowok <nowok.lukas@gmail.com>
2023-06-08 11:56:31 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,23 @@
 // must be in HTTPS
+
+let osc;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   console.log('starting');
-	noStroke();
-  
-  fill(255);
+	stroke(0);
+
   textSize(18);
   text("animalcity", height/3);
+
+  osc = new p5.Oscillator('sine');
+  osc.amp(0.2);
+  osc.freq(200);
+
   // get position once
   if(geoCheck() == true) {
      background(135, 200, 118);
+     osc.start();
   } else {
      background(234, 51, 35);
   }
@@ -20,6 +28,10 @@ function positionChanged(position){
     print("lat: " + position.latitude);
     print("long: " + position.longitude);
     text(nf(position.latitude,2,8) + " " + nf(position.longitude,2,8), 10, height/2);
+}
+
+function positionToFrequency(){
+  
 }
 
 /*
